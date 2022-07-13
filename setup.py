0a4721e20e559de5161a94372c29d784ba92a60e
mrkouchaki <93693562+mrkouchaki@users.noreply.github.com>
2022-07-13 17:05:56 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     packages=find_packages(exclude=["tests.*", "tests"]),
     description="mr xApp for test",
     url="https://github.com/mrkouchaki/mr-xapp",
-    install_requires=["ricxappframe>=1.1.1,<2.0.0", "pandas>=1.1.3", "joblib>=0.3.2", "Scikit-learn>=0.21", "schedule>=0.0.0", "influxdb"],
+    install_requires=["ricxappframe>=1.1.1,<2.0.0", "pandas>=1.1.3", "joblib>=0.3.2", "Scikit-learn>=0.21", "schedule>=0.0.0", "influxdb", "p5py", "PEP517", "Cython", "numpy >= 1.16.2", "pandas>=1.1.3", "torch==1.9.0", "torchvision==0.10.0", "torchaudio==0.9.0","tensorflow"],
     entry_points={"console_scripts": ["run-mr.py=mr.main:start"]},  # adds a magical entrypoint for Docker
     license="Apache 2.0",
     data_files=[("", ["LICENSE.txt"])],
