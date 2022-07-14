a6f0fe5fd617e54399ef731e9718ae4e9821c780
mrkouchaki <93693562+mrkouchaki@users.noreply.github.com>
2022-07-14 12:06:57 -0500

    Update db.py

--- mr/db.py
+++ mr/db.py
@@ -16,7 +16,7 @@ import datetime
 
 
 class DBCreateDrop:
-
+    print('///////////enter class DBCreatDrop\\\\\\\\\\\\')
     def __init__(self, dbname):
         print('enter insert init')
         host = 'localhost'
@@ -49,6 +49,7 @@ class DBCreateDrop:
 #class DATABASE(object):
 
 class DATReadWrite(object):
+    print('///////////enter class DATReadWrite\\\\\\\\\\\\')
     r""" DATABASE takes an input as database name. It creates a client connection
       to influxDB and It reads/ writes UE data for a given dabtabase and a measurement.
 
@@ -115,6 +116,7 @@ class DATReadWrite(object):
         self.client.write_points(df, meas)
 
 class DATABASE(object):
+    print('///////////enter class DATABASE(object)\\\\\\\\\\\\')
     r""" DATABASE takes an input as database name. It creates a client connection
       to influxDB and It reads/ writes UE data for a given dabtabase and a measurement.
     Parameters
@@ -136,36 +138,49 @@ class DATABASE(object):
     """
 
     def __init__(self, dbname, user='root', password='root', host="r4-influxdb.ricplt", port='8086'):
+        print('///////enter def __init__ in class DATABASE\\\\\\\\\\')
         self.data = None
         self.client = DataFrameClient(host, port, user, password, dbname)
+        print('self.client=', self.client)
 
     def read_data(self, meas, limit=100):
+        print('///////enter def read_data(self, meas, limit=100): in class DATABASE\\\\\\\\\\')
         """Read data method for a given measurement and limit
         Parameters
         ----------
         meas: str (default='cellMeasReport')
         limit:int (defualt=100)
         """
-
+        print('meas=', meas)
+        print('limit=', limit)
         result = self.client.query('select * from ' + meas + ' limit ' + str(limit))
+        print('result=', result)
         print("Querying data : " + meas + " : size - " + str(len(result[meas])))
         try:
+            print('result[meas]=', result[meas])
+            print('len(result[meas])=', len(result[meas]))
             if len(result[meas]) != 0:
                 self.data = result[meas]
+                print('self.data==', self.data)
                 self.data['measTimeStampRf'] = self.data.index
+                print('self.data[measTimeStampRf]=', self.data['measTimeStampRf'] )
             else:
+                print('else:')
                 raise NoDataError
 
         except NoDataError:
+            print('except NODataErro:')
             print('Data not found for ' + meas + ' vnf')
 
     def write_lp_prediction(self, df, meas='LP'):
+        print('///////enter def write_lp_prediction(self, df, meas=LP): in class DATABASE\\\\\\\\\\')
         """Write data method for a given measurement
         Parameters
         ----------
         meas: str (default='LP')
         """
         self.client.write_points(df, meas)
+        print('self.client.write_points(df, meas)=', self.client.write_points(df, meas))
 
 # In[ ]:
 
@@ -187,18 +202,27 @@ class DATABASE(object):
 #ad:
 
 class DUMMY:
+    print('///////////enter class DUMMY in db\\\\\\\\\\\\\\')
 
     def __init__(self):
-        self.ue = pd.read_csv('ad/valid.csv')
+        print('///////enter def __init__ in class DUMMY\\\\\\\\\\')
+        self.ue = pd.read_csv('mr/valid.csv')
+        print('self.ue= pd.read_csv(mr/valid.csv)=', self.ue)
         self.data = None
 
     def read_data(self, meas='ueMeasReport', limit=100000):
+        print('///////enter def read_data(self, meas=ueMeasReport, limit=100000): in class DATABASE\\\\\\\\\\')
+        print('meas=', meas)
         if meas == 'valid':
+            print('meas == valid:')
             self.data = self.ue.head(limit)
+            print('self.data = self.ue.head(limit)=', self.data)
         else:
             self.data = self.ue.head(limit).drop('Anomaly', axis=1)
+            print('self.data = self.ue.head(limit).drop(Anomaly, axis=1)=', self.data)
 
     def write_anomaly(self, df, meas_name='QP'):
+        print('///////enter def write_anomaly(self, df, meas_name=QP): in class DATABASE\\\\\\\\\\')
         pass
 
     
@@ -207,11 +231,13 @@ class DUMMY:
 
 
 class Error(Exception):
+    print('////////enter class Error in db\\\\\\\\\\')
     """Base class for other exceptions"""
     pass
 
 
 class NoDataError(Error):
+    print('////////enter class NoDataError in db\\\\\\\\\\')
     """Raised when there is no data available in database for a given measurment"""
     pass
 
